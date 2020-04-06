0ab30dfe2bd007522600c916860e8c764a209230
ynwd <ynwd@users.noreply.github.com>
2020-04-06 11:03:11 +0700

    add npm install command

--- index.js
+++ index.js
@@ -6,3 +6,4 @@ const cwd = process.cwd();
 const gitCommand = `git clone git@github.com:fastrolib/fastro-web.git ${cwd}`;
 execSync(gitCommand, {stdio: [0, 1, 2], cwd});
 execSync('rm -rf .git', {stdio: [0, 1, 2], cwd});
+execSync('npm install', {stdio: [0, 1, 2], cwd});
