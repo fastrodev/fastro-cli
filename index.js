a33886a07339a6712b50011f5d19c8269ea81f1e
ynwd <yanuwid@gmail.com>
2020-04-14 10:36:31 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const path = require('path');
 const{ execSync } = require('child_process');
 const cwd = process.cwd();
-const gitCommand = `git clone git@github.com:fastrolib/fastro-web.git ${cwd}`;
+const gitCommand = `git clone git@github.com:fastrodev/fastro-web.git ${cwd}`;
 execSync(gitCommand, {stdio: [0, 1, 2], cwd});
 execSync('rm -rf .git', {stdio: [0, 1, 2], cwd});
 execSync('npm install', {stdio: [0, 1, 2], cwd});
